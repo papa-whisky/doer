35b95c40eb560fe4edc70daa693613307f3b49cf
Paul Williams <paul@pdwilliams.net>
2016-10-10 12:03:05 +1100

    add library

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+console.log('this is the app.js');
+console.log(aVariable);
+
 var addBtn = document.getElementById('add-btn');
 var newTodoInput = document.getElementById('new-todo-input');
 var todoList = document.getElementById('todo-list');
@@ -5,9 +8,23 @@ var todoList = document.getElementById('todo-list');
 var addTodo = function() {
   var newTodoContent = newTodoInput.value;
   var newListItem = document.createElement('li');
-  var newContent = document.createTextNode(newTodoContent);
+  var newContent = document.createTextNode(newTodoContent); // Do this instead of innerHTML for compatability with old IE.
   newListItem.appendChild(newContent);
+
+  newListItem.addEventListener('click', function(event) {
+    // console.log(event.target);
+    event.target.className = 'done';
+  })
+
   todoList.appendChild(newListItem);
+  newTodoInput.value = ''; // Clear input.
+
 }
 
 addBtn.addEventListener('click', addTodo);
+
+var listItems = document.getElementsByTagName('li');
+
+// for (var i = 0; i < listItems.length; i++) {
+
+// }
